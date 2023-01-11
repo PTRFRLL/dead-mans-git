26db77f4d9f78cf5ad898e8ffa17eb2050a07bbe
Peter Fiorella <github@fiorella.me>
2023-01-11 12:53:22 -0700

    Test env var

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 async function run() {
   console.log(`Function was run at ${new Date().toLocaleString()}`);
+  const repo = process.env.REPO_URL;
+  if (!repo) {
+    console.error("REPO_URL not set");
+    process.exit(1);
+  }
+  console.log({ repo });
 }
 
 run();
